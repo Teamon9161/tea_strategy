fee028788809d492f6a4e8bb0d14abe3cf8450a5
Teamon9161 <1127972945@qq.com>
2024-04-26 15:44:01 +0800

    remove comment

--- src/strategy_filter.rs
+++ src/strategy_filter.rs
@@ -21,13 +21,3 @@ impl<T: Vec1View<Item = Option<bool>>> StrategyFilter<T> {
         TrustIter::new(iter, self.long_open.len())
     }
 }
-// impl<'a> StrategyFilter<'a> {
-//     fn from_inputs(inputs: &'a [Series], idxs: (usize, usize, usize, usize)) -> PolarsResult<Self> {
-//         Ok(Self {
-//             long_open: inputs[idxs.0].bool()?,
-//             long_stop: inputs[idxs.1].bool()?,
-//             short_open: inputs[idxs.2].bool()?,
-//             short_stop: inputs[idxs.3].bool()?,
-//         })
-//     }
-// }
